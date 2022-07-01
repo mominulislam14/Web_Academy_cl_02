15b3c5e458cfae3a49c41c3ede925757503aa8bb
mominulislam14 <102807312+mominulislam14@users.noreply.github.com>
2022-07-02 03:32:01 +0600

    js work

---
+++ app.js
@@ -0,0 +1,20 @@
+// Number, String, object, boolean, undefined, null
+// var, let, const
+// String
+var x = "Hello World,world";
+var y = "WebAcademy";
+//var y = new String("Hello World");
+
+//let z = x.slice(-5);
+// let z = x.slice(0, 5);
+// let a = x.substring(0, 5);
+// console.log(z);
+// console.log(a);
+
+//let z = x.replace(/World/g, "WebAcademy");
+// let z = x.toUpperCase();
+//let z = x.toLowerCase();
+// let z = x.concat(" ", y);
+//let z = x.trim();
+let z = x.split(",");
+console.log(z);
